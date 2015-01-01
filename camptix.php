d47b12f9f7aa2670444b0e78629adc271c37be4f
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-01 00:46:31 +0000

    Sync with GitHub master.
    git-svn-id: https://plugins.svn.wordpress.org/camptix/trunk@1057708 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- camptix.php
+++ camptix.php
@@ -4676,7 +4676,7 @@ class CampTix_Plugin {
 									<?php if ( $price > 0 ) : ?>
 										<?php echo $this->append_currency( $price ); ?>
 									<?php else : ?>
-										Free
+										<?php _e( 'Free', 'camptix' ); ?>
 									<?php endif; ?>
 								</td>
 								<?php if ( apply_filters( 'camptix_show_remaining_tickets', true ) ) : ?>
@@ -4836,7 +4836,7 @@ class CampTix_Plugin {
 								<?php if ( $price > 0 ) : ?>
 									<?php echo $this->append_currency( $price ); ?>
 								<?php else : ?>
-									Free
+									<?php _e( 'Free', 'camptix' ); ?>
 								<?php endif; ?>
 								</td>
 								<td class="tix-column-quantity"><?php echo intval( $count ); ?></td>
@@ -5548,7 +5548,7 @@ class CampTix_Plugin {
 	 * Looks for the [camptix] page and returns the page's id.
 	 */
 	function get_tickets_post_id() {
-		$posts = get_posts( array(
+		$params = apply_filters( 'camptix_get_tickets_post_id_params', array(
 			'post_type' => 'page',
 			'post_status' => 'publish',
 			's' => '[camptix',
@@ -5556,6 +5556,7 @@ class CampTix_Plugin {
 			'update_post_term_cache' => false,
 			'update_post_meta_cache' => false,
 		) );
+		$posts = get_posts( $params );
 
 		if ( ! $posts )
 			return false;
@@ -5706,7 +5707,7 @@ class CampTix_Plugin {
 			$remaining -= $reserved_tickets;
 		}
 
-		return $remaining;
+		return apply_filters( 'camptix_get_remaining_tickets', $remaining, $post_id, $via_reservation, $quantity, $reservations );
 	}
 
 	function get_purchased_tickets_count( $post_id, $via_reservation = false ) {
